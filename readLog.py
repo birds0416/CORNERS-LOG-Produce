054bf06cb2497b65e1b2bf9cc60bea49e119a8c8
birds0416 <birds8277@gmail.com>
2023-03-18 23:21:05 +0900

    readLog.py Add

--- readLog.py
+++ readLog.py
@@ -1,21 +1,52 @@
 import pandas as pd
+import json
 
 def readLog(logPath):
     logFile = open(logPath, 'rt', encoding='UTF8')
 
     analysis = []
+    temp = []
+    isAnalyze = False
 
     for i, line in enumerate(logFile):
-
-        temp = []
-        # time = line[1:24]
-        # allInfo = line[33:].split(', ')
-        # print(allInfo)
-        if line[33:54] != "=== START ANALIZE ===" and i != 0:
+        if line[33:54] == "=== START ANALIZE ===":
+            isAnalyze = True
+            if temp != []:
+                analysis.append(temp)
+                temp = []
             temp.append(line)
 
+        else:
+            isAnalyze = False
+        
+        if isAnalyze == False and i != 0:
+            temp.append(line)
+    
+    return analysis
 
 
 
-path = "D:/Corners/대상WL 로그재현 Tool/log/DetectManager20230317.log"
-readLog(path)
+
+'''
+    readLog.py TEST
+    Analysis 리스트에 잘 들어가는 거 확인했음
+'''
+# path = "D:/Corners/대상WL 로그재현 Tool/log/DetectManager20230317.log"
+# analysis = readLog(path)
+# newJson = {"Analysis Test":[]}
+# json_obj = json.dumps(newJson, indent=4)
+# with open("analysis_test.json", 'w') as outfile:
+#     outfile.write(json_obj)
+
+# with open("analysis_test.json", 'r+') as file:
+#     file_data = json.load(file)
+#     for item in analysis:
+#         file_data["Analysis Test"].append(item)
+    
+#     file.seek(0)
+#     json.dump(file_data, file, indent=4)
+
+
+
+    
+    
